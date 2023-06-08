91a98ad4a3c3452defaa3a564872916448bfec09
MatusOllah <matusollah420@gmail.com>
2023-06-08 14:28:41 +0200

    add --version flag

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/fatih/color"
 	"github.com/jessevdk/go-flags"
 	"github.com/ztrue/tracerr"
 )
 
+const version = "1.0.0"
+
 type Config struct {
 	Verbose   bool   `short:"v" long:"verbose" description:"Show verbose debug information"`
 	Path      string `short:"p" long:"path" description:"Path to FNF mod" default:"."`
 	Color     bool   `short:"c" long:"color" description:"Color output"`
 	ShowLines bool   `short:"l" long:"show-lines" description:"Show lines when printing found instances"`
+	Version   bool   `long:"version" description:"Print version and exit"`
 }
 
 var cfg Config
@@ -22,6 +26,21 @@ func main() {
 	if _, err := flags.NewParser(&cfg, flags.HelpFlag|flags.PassDoubleDash).Parse(); err != nil {
 		tracerr.Print(err)
 		os.Exit(1)
+	}
+
+	c := color.New(color.FgCyan, color.Bold).SprintfFunc()
+	w := color.New(color.FgWhite, color.Bold).SprintfFunc()
+
+	if cfg.Version {
+		if cfg.Color {
+			fmt.Fprintln(color.Output, c("bfcheck"), w("version"), version)
+			fmt.Fprintln(color.Output, c("Go"), w("version"), runtime.Version())
+		} else {
+			fmt.Println("bfcheck version", version)
+			fmt.Println("Go version", runtime.Version())
+		}
+
+		os.Exit(0)
 	}
 
 	num, err := checkDir(cfg.Path)
